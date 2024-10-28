85d3e47f1405237407d8b0180a366b6ed9eb2fca
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2024-10-28 09:05:53 +0100

    Add types folder

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.jsonc' {
-  const value: any
-  export default value
-}
-
-declare module '*.json5' {
-  const value: any
-  export default value
-}
---
+++ types/json5.d.ts
@@ -0,0 +1,4 @@
+declare module '*.json5' {
+  const value: any
+  export default value
+}

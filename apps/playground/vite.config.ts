2644896bb18277932da47e5a41003a39a26135ec
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2026-04-23 11:17:33 +0200

    refactor: enhance playground configuration and update imports
    - Added a new TypeScript configuration file for the playground.
    - Updated ESLint configuration to include the new TypeScript project.
    - Modified Vite configuration to use path aliases for fixture imports.
    - Refactored imports in Playground.vue to utilize the new alias.
    - Removed unused JSON5 and JSONC example files.

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,7 +1,13 @@
+import { resolve } from 'path'
 import vuePlugin from '@vitejs/plugin-vue'
 import json5Plugin from 'vite-plugin-json5'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vuePlugin(), json5Plugin()]
+  plugins: [vuePlugin(), json5Plugin()],
+  resolve: {
+    alias: {
+      '@fixtures': resolve(__dirname, '../../packages/vite-plugin-json5/tests/json')
+    }
+  }
 })

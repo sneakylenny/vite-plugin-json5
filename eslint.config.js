2d0d675c26e35d74e98986ea50cdd9248fbebaf7
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2026-04-24 09:57:04 +0200

    chore: update ESLint configuration
    - Migrated ESLint configuration to use the new defineConfig format, integrating TypeScript, Vue, and stylistic plugins.
    - Added new devDependencies for ESLint, stylistic, and Vue plugins in package.json.
    - Updated pnpm-lock.yaml to reflect the new dependencies and their versions.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,22 @@
-import neostandard from 'neostandard'
+// @ts-check
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import eslintPluginVue from 'eslint-plugin-vue'
+import stylistic from '@stylistic/eslint-plugin'
 
-export default [
-  { ignores: ['dist/**/*'] },
-  ...neostandard({ ts: true }),
-]
+export default defineConfig([
+  tseslint.configs.strict,
+  eslintPluginVue.configs['flat/recommended'],
+  stylistic.configs.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    ignores: ['dist/**/*'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.base.json', './apps/*/tsconfig.json', './packages/*/tsconfig.json'],
+      },
+    },
+  },
+])
